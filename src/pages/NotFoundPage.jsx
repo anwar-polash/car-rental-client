378dcd7c3080e178cd96a32311e0782ab1c8b381
anwar-polash <coder.polash@gmail.com>
2025-01-19 12:29:13 +0600

    not found page

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+const NotFoundPage = () => {
+  document.title = "404 Not Found | Chill Gamer";
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="max-w-2xl w-full bg-blue-100 rounded-lg shadow-lg p-10 flex flex-col gap-16 items-center mb-20">
+        <h1 className="text-3xl font-semibold text-center">404 Not Found</h1>
+        <p className="text-center">
+          The page you are looking for does not exist.
+        </p>
+        <Link
+          to="/"
+          className="px-4 py-2 bg-[#FFC147] text-white rounded hover:bg-[#FFC147]">
+          Back to Home
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
