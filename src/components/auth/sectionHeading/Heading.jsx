e02da10022049280c5240dac832c7d9c938d8b4b
anwar-polash <coder.polash@gmail.com>
2025-01-19 12:19:34 +0600

    section heading

---
+++ src/components/auth/sectionHeading/Heading.jsx
@@ -0,0 +1,9 @@
+const Heading = ({ children }) => {
+  return (
+    <h2 className="text-center text-4xl capitalize font-bold py-[2rem] ">
+      {children}
+    </h2>
+  );
+};
+
+export default Heading;
