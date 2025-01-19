7bb7c43fa7826e87c0a23f3c4b067413f8d90e86
anwar-polash <coder.polash@gmail.com>
2025-01-19 12:26:52 +0600

    add car page

---
+++ src/pages/AddCarPage.jsx
@@ -0,0 +1,11 @@
+import AddCarForm from '../components/AddCarForm';
+
+const AddCarPage = () => {
+  return (
+    <section className="min-h-screen">
+      <AddCarForm />
+    </section>
+  );
+};
+
+export default AddCarPage;
